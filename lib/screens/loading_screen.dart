b3259800c3d92c85bffee57a06ecb982b7e7b91f
PapailleEnQueue <contact@nicolas-broders.info>
2020-09-01 21:51:20 +0200

    Use API weather with smartphone location

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,4 +1,8 @@
+import 'package:clima/services/networking.dart';
 import 'package:flutter/material.dart';
+import '../services/location.dart';
+
+const apiKey = '4629f92b5d10f9c309d07b6b203f5082';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -6,17 +10,36 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  Location location = Location();
+  double latitude;
+  double longitude;
+
+  void getLocationData() async {
+    await location.getActualLocation();
+    latitude = location.latitude;
+    longitude = location.longitude;
+
+    NetworkingHelper networkingHelper = NetworkingHelper(
+        'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey');
+    var weatherData = await networkingHelper.getData();
+
+    double temperature = weatherData['main']['temp'];
+    int condition = weatherData['weather'][0]['id'];
+    String cityName = weatherData['name'];
+
+    print(temperature);
+    print(condition);
+    print(cityName);
+  }
+
+  @override
+  void initState() {
+    getLocationData();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            //Get the current location
-          },
-          child: Text('Get Location'),
-        ),
-      ),
-    );
+    return Scaffold();
   }
 }
